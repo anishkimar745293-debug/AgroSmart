7df9bab2a81b76a6692501a58fe0765bac919bfa
Kumar Anish <anishkimar745293@gmail.com>
2026-07-10 11:29:11 +0530

    Update AuthButton.jsx

--- agrosmart/src/components/auth/AuthButton.jsx
+++ agrosmart/src/components/auth/AuthButton.jsx
@@ -1,17 +1,22 @@
+// components/auth/AuthButton.jsx
 const AuthButton = ({
   text,
   disabled = false,
+  role = "farmer", // Default role farmer rakha hai
+  onClick
 }) => {
+  // Role ke hisab se button ka color set hoga
+  const buttonColor = role === "expert" 
+    ? "bg-blue-600 hover:bg-blue-700 text-white" 
+    : "bg-green-600 hover:bg-green-700 text-white";
+
   return (
     <button
-      type="submit"
+      type={onClick ? "button" : "submit"}
       disabled={disabled}
-      className={`w-full rounded-xl py-4 font-semibold transition
-
-      ${
-        disabled
-          ? "bg-gray-400 cursor-not-allowed"
-          : "bg-green-600 hover:bg-green-700 text-white"
+      onClick={onClick}
+      className={`w-full rounded-xl py-4 font-semibold transition cursor-pointer shadow-md ${
+        disabled ? "bg-gray-400 cursor-not-allowed text-white" : buttonColor
       }`}
     >
       {text}
